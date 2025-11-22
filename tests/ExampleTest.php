17e7509f57efc47f6df047540b014edad69f16b0
msaad23305@gmail.com <msaad23305@gmail.com>
2025-11-23 04:14:42 +0500

    feat: Configure full CI/CD pipeline with testing and deployment

---
+++ tests/ExampleTest.php
@@ -0,0 +1,11 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function test_it_asserts_that_true_is_true(): void
+    {
+        $this->assertTrue(true);
+    }
+}
